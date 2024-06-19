cea78a936d4c3912e0bbeccbc78485e06f39fc84
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-19 10:09:12 +0600

    added video background

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-function replaceVideo(imageUrl) {
+function injectImage(imageUrl) {
   const videoContainer = document.querySelector('div.framer-39t0w6-container');
   if (videoContainer) {
     videoContainer.innerHTML = `<img src="${imageUrl}" style="width: 100%; height: 100%; object-fit: cover; object-position: center center;" />`;
@@ -7,7 +7,22 @@ function replaceVideo(imageUrl) {
   return false;
 }
 
-function replaceHtml(html) {
+function injectVideo(videoUrl, autoplay = "", loop = "", muted = "", controls = "") {
+  const youtubeVideoId = videoUrl.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/);
+  const videoContainer = document.querySelector('div.framer-39t0w6-container');
+  if (videoContainer) {
+    if (youtubeVideoId) {
+      let content = `<iframe src="https://www.youtube.com/embed/${youtubeVideoId[1]}?autoplay=${autoplay ? 1 : 0}&loop=${loop ? 1 : 0}&mute=${muted ? 1 : 0}&controls=${controls ? 1 : 0}${loop ? `&playlist=${youtubeVideoId}` : ""}" style="width: 100%; height: 100%; object-fit: cover; object-position: center center;" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />`;
+      videoContainer.innerHTML = content;
+      return true;
+    }
+    videoContainer.innerHTML = `<video src="${videoUrl}" ${autoplay} ${loop} ${muted} ${controls} style="width: 100%; height: 100%; object-fit: cover; object-position: center center;" />`;
+    return true;
+  }
+  return false;
+}
+
+function injectHtml(html) {
   const videoContainer = document.querySelector('div.framer-39t0w6-container');
   if (videoContainer) {
     videoContainer.innerHTML = html;
@@ -31,40 +46,70 @@ function init() {
     }
   });
 
-  chrome.storage.sync.get('image', (data) => {
-    const imageUrl = data.image;
-    if (imageUrl && imageUrl.trim() !== '') {
-      if (!replaceVideo(imageUrl)) {
-        const observer = new MutationObserver((mutations, obs) => {
-          if (replaceVideo(imageUrl)) {
-            obs.disconnect();
-          }
-        });
-        observer.observe(document.body, { childList: true, subtree: true });
+  new Promise((resolve) => {
+    chrome.storage.sync.get('image', (data) => {
+      const imageUrl = data.image;
+      if (imageUrl && imageUrl.trim() !== '') {
+        if (!injectImage(imageUrl)) {
+          const observer = new MutationObserver((mutations, obs) => {
+            if (injectImage(imageUrl)) {
+              obs.disconnect();
+            }
+          });
+          observer.observe(document.body, { childList: true, subtree: true });
+        }
       }
-    }
-  });
-
-  chrome.storage.sync.get('html', (data) => {
-    const html = data.html;
-    if (html && html.trim() !== ''){
-      if (!replaceHtml(html)) {
-        const observer = new MutationObserver((mutations, obs) => {
-          if (replaceHtml(html)) {
-            obs.disconnect();
+      resolve();
+    });
+  })
+  .then(() => {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get('video', (data) => {
+        const videoUrl = data.video;
+      
+        Promise.all([
+          new Promise((resolve) => chrome.storage.sync.get('autoplay', (data) => resolve(data.autoplay))),
+          new Promise((resolve) => chrome.storage.sync.get('loop', (data) => resolve(data.loop))),
+          new Promise((resolve) => chrome.storage.sync.get('muted', (data) => resolve(data.muted))),
+          new Promise((resolve) => chrome.storage.sync.get('controls', (data) => resolve(data.controls)))
+        ]).then(([autoplay, loop, muted, controls]) => {
+          if (videoUrl && videoUrl.trim() !== '') {
+            if (!injectVideo(videoUrl, autoplay, loop, muted, controls)) {
+              const observer = new MutationObserver((mutations, obs) => {
+                if (injectVideo(videoUrl, autoplay, loop, muted, controls)) {
+                  obs.disconnect();
+                }
+              });
+              observer.observe(document.body, { childList: true, subtree: true });
+            }
           }
+          resolve();
         });
-        observer.observe(document.body, { childList: true, subtree: true });
+      });
+    });
+  })
+  .then(() => {
+    chrome.storage.sync.get('html', (data) => {
+      const html = data.html;
+      if (html && html.trim() !== ''){
+        if (!injectHtml(html)) {
+          const observer = new MutationObserver((mutations, obs) => {
+            if (injectHtml(html)) {
+              obs.disconnect();
+            }
+          });
+          observer.observe(document.body, { childList: true, subtree: true });
+        }
       }
-    }
+    });
   });
 }
 
 chrome.storage.onChanged.addListener(init);
 chrome.runtime.onMessage.addListener((message) => {
   init();
-  const { title, image, html } = message;
-  ((!image && !html) || !title) && location.reload();
+  const { title, image, video, html } = message;
+  ((!image && !html && !video) || !title) && location.reload();
 });
 
 window.addEventListener('load', init, false);
